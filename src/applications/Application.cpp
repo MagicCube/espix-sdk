592605ac93f83780330bce13c6ab6ed35b4f4d9e
Henry Li <henry1943@163.com>
2018-08-19 13:02:50 +0800

    Add keyboard support

--- src/applications/Application.cpp
+++ src/applications/Application.cpp
@@ -2,16 +2,39 @@
 
 Application::Application(OLEDDisplay *display) {
   _screen = new Screen(display);
+  _keyboard = new Keyboard();
 }
 
 Screen *Application::getScreen() {
   return _screen;
 }
 
+Keyboard *Application::getKeyboard() {
+  return _keyboard;
+}
+
+void Application::onKeyPress(KeyEventHandler onKeyPress) {
+  _onKeyPress = onKeyPress;
+}
+
 void Application::begin() {
   _screen->begin();
+
+  _keyboard->begin();
+  _keyboard->onKeyPress([=](KeyCode keyCode) { _handleKeyPress(keyCode); });
 }
 
 int Application::update() {
   return 100;
+}
+
+
+void Application::_handleKeyPress(KeyCode keyCode) {
+  _fireKeyPressEvent(keyCode);
+}
+
+void Application::_fireKeyPressEvent(KeyCode keyCode) {
+  if (_onKeyPress != NULL) {
+    _onKeyPress(keyCode);
+  }
 }
--- src/applications/Application.h
+++ src/applications/Application.h
@@ -5,6 +5,7 @@
 #include <OLEDDisplay.h>
 
 #include "../drawings/Screen.h"
+#include "../hmis/Keyboard.h"
 
 // Represent the application of Expix.
 class Application {
@@ -12,15 +13,28 @@ public:
   // Construct an Espix application.
   Application(OLEDDisplay *display);
 
+  // Get a Screen object represent the current screen.
+  Screen *getScreen();
+
+  // Get a Keyboard object represent the current keyboard.
+  Keyboard *getKeyboard();
+
+  // Fires when key pressed.
+  void onKeyPress(KeyEventHandler onKeyPress);
+
   // Initialize application. Place this method in your setup().
   void begin();
 
   // Update application in the Arduino loop. Place this method in your loop().
   int update();
 
-  // Get a Screen object represent the current screen.
-  Screen *getScreen();
 
 private:
   Screen *_screen;
+  Keyboard *_keyboard;
+
+  KeyEventHandler _onKeyPress;
+
+  void _handleKeyPress(KeyCode keyCode);
+  void _fireKeyPressEvent(KeyCode keyCode);
 };
