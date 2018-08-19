7da1e53f68b3d660ba7b5d746028397641713e46
Henry Li <henry1943@163.com>
2018-08-19 18:15:36 +0800

    Add TextView.h

---
+++ src/views/TextView.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <Arduino.h>
+
+#include "View.h"
+
+class TextView : public View {
+public:
+  TextView(String text, FONT_SIZE fontSize = FONT_SIZE_H1,
+           TEXT_ALIGN textAlign = TEXT_ALIGN_CENTER_BOTH) {
+    _text = text;
+    _fontSize = fontSize;
+    _textAlign = textAlign;
+  }
+
+  void setText(String text) {
+    if (!text.equals(_text)) {
+      _text = text;
+      setDirty();
+    }
+  }
+
+  void render(DrawingContext *context) {
+    Serial.println("TextView::render()");
+    context->setFontSize(_fontSize);
+    context->setTextAlign(_textAlign);
+    context->drawString(_text);
+  }
+
+private:
+  String _text;
+  TEXT_ALIGN _textAlign;
+  FONT_SIZE _fontSize;
+};
