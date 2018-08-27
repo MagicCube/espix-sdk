a6cb5c6242a703aa4f7a713d462c2860462dbc37
亨睿 <henry.lx@alibaba-inc.com>
2018-08-27 16:12:50 +0800

    Add Rectangle.h

---
+++ src/espix-core/drawing/Rectangle.h
@@ -0,0 +1,22 @@
+#pragma once
+
+struct Rectangle {
+  Rectangle(int p_left = 0, int p_top = 0, int p_width = 0, int p_height = 0) {
+    left = p_left;
+    top = p_top;
+    width = p_width;
+    height = p_height;
+  }
+
+  Rectangle(int p_width = 0, int p_height = 0) {
+    left = 0;
+    top = 0;
+    width = p_width;
+    height = p_height;
+  }
+
+  int left = 0;
+  int top = 0;
+  int width = 0;
+  int height = 0;
+};
