4714ed1ca2369d4393331ca46d0d621939c0c126
亨睿 <henry.lx@alibaba-inc.com>
2018-08-27 18:00:22 +0800

    Update

--- src/espix-core/devices/Screen.h
+++ src/espix-core/devices/Screen.h
@@ -7,53 +7,48 @@
 #include "../drawing/Canvas.h"
 #include "../drawing/CanvasContext.h"
 
-// Represent an OLED screen, you use this class to get screen resolution or set brightness.
+// Represents a screen.
 class ScreenClass {
 public:
-  // Construct a Screen object by passing an OLEDDisplay object.
+  // Initializes a new instance of the Screen class.
   ScreenClass();
 
-  // Get internal OLEDDisplay object.
+  // Gets the internal OLEDDisplay object.
   OLEDDisplay *getDisplay();
 
-  // Get canvas of screen.
+  // Gets the canvas of screen.
   Canvas *getCanvas();
 
-  // Get brightness percentage of the screen. The range of percentage is 0-100.
+  // Gets or sets brightness percentage of the screen. The range of percentage is 0-100.
   uint8_t getBrightness();
-
-  // Set brightness percentage of the screen. The range of percentage is 0-100.
   void setBrightness(uint8_t percentage);
 
-  // Return true if the screen is flipped vertically.
+  // Gets true if the screen is flipped vertically.
   bool isFlipped();
 
-  // Set whether the screen is flipped and mirrored.
+  // Sets whether the screen is flipped and mirrored.
   void setOrientation(bool flipped = false, bool mirrored = false);
 
-  // Return true if the screen is mirrored horizontally.
+  // Gets true if the screen is mirrored horizontally.
   bool isMirrored();
 
-  // Get width of the screen.
+  // Gets width of the screen.
   int getWidth();
 
-  // Get height of the screen.
+  // Gets height of the screen.
   int getHeight();
 
-  // Initialize screen.
+  // Initializes screen.
   void begin(OLEDDisplay *display);
 
-  // Update in loop().
+  // Updates in loop().
   void update();
 
-  // Clear buffer of screen.
+  // Clears buffer of screen.
   void clearBuffer();
 
-  // Clear screen immediately.
+  // Clears screen immediately.
   void clear();
-
-  // Create a new CanvasContext object.
-  CanvasContext *createCanvasContext();
 
 private:
   OLEDDisplay *_display;
