74dcdfcc823c98ef336b55df9f426939cc5b16e9
Henry Li <henry1943@163.com>
2018-08-27 20:41:06 +0800

    Add comments

--- src/espix-core/timing/TimeClient.h
+++ src/espix-core/timing/TimeClient.h
@@ -4,19 +4,34 @@
 
 #include "DateTime.h"
 
+// Represents a automatically synchronized NTP client.
+// The only instance of TimeClientClass is a global variable `TimeClient`.
 class TimeClientClass {
 public:
+  // Initializes a new instance of the TimeClientClass class.
+  // Do not call this constructor directly. Always use the global variable `TimeClient`.
   TimeClientClass(unsigned long timeOffset);
 
+  // Gets the Unix timestamp of now.
   unsigned long now();
+
+  // Gets a boolean value indicates whether the time client is synchronized.
   bool isReady();
+
+  // Get a DateTime object in local time zone.
   DateTime getLocalTime();
+
   // Get formatted local time.
   // http://www.cplusplus.com/reference/ctime/strftime/
   String getLocalTimeStrig();
 
+  // Initializes the time client.
   void begin();
+
+  // Updates in the loop.
   void update();
+
+  // Forces to update online.
   void forceUpdate();
 
 private:
