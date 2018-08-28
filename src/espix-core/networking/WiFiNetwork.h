e8188f6bccc674edbb99a172d01c07e9b6402d9a
Henry Li <henry1943@163.com>
2018-08-28 20:03:18 +0800

    Use MutableList

--- src/espix-core/networking/WiFiNetwork.h
+++ src/espix-core/networking/WiFiNetwork.h
@@ -4,7 +4,7 @@
 
 #include <ESP8266WiFi.h>
 
-#include "../collections/List.h"
+#include "../collections/MutableList.h"
 
 #include "WiFiConnectionSetting.h"
 #include "WiFiConnectionState.h"
@@ -37,7 +37,7 @@ private:
   void _scan();
 
   WiFiConnectionState _connectionState = WiFiConnectionState::DISCONNECTED;
-  List<WiFiConnectionSetting> _preferredList;
+  MutableList<WiFiConnectionSetting> _preferredList;
 };
 
 extern WiFiNetworkClass WiFiNetwork;
