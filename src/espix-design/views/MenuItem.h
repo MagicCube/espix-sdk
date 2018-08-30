6ed5d333e209d6b526d2a0856fb4eee680a46f10
Henry Li <henry1943@163.com>
2018-08-30 22:45:21 +0800

    Add constructors

--- src/espix-design/views/MenuItem.h
+++ src/espix-design/views/MenuItem.h
@@ -3,6 +3,12 @@
 #include <Arduino.h>
 
 struct MenuItem {
+  MenuItem(String p_text) : key(p_text), text(p_text) {
+  }
+
+  MenuItem(String p_key, String p_text) : key(p_key), text(p_text) {
+  }
+
   String key;
   String text;
 };
