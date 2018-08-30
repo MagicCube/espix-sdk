aad52d030c1f65f7136eae08a7628ce04706f603
Henry Li <henry1943@163.com>
2018-08-30 23:09:01 +0800

    Change padding

--- src/espix-core/views/NavigationContainer.cpp
+++ src/espix-core/views/NavigationContainer.cpp
@@ -33,8 +33,8 @@ void NavigationContainer::showStatusView() {
   }
   _statusViewVisible = true;
   _statusView->setWidth(getWidth());
-  _statusView->setTop(-_statusView->getHeight());
-  setPaddings(Thickness(0, _statusView->getHeight(), 0, 0));
+  _statusView->setTop(-_statusView->getHeight() - 1);
+  setPaddings(Thickness(0, _statusView->getHeight() + 1, 0, 0));
 }
 
 void NavigationContainer::hideStatusView() {
