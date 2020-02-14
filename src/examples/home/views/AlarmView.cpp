883d059eea22749595841b577394dd7d49efe211
亨睿 <henry.lx@alibaba-inc.com>
2020-02-14 20:47:32 +0800

    Add AlarmView

---
+++ src/examples/home/views/AlarmView.cpp
@@ -0,0 +1,119 @@
+#include "AlarmView.h"
+
+AlarmView::AlarmView() {
+}
+
+AlarmView *AlarmView::getInstance() {
+  static AlarmView instance;
+  return &instance;
+}
+
+void AlarmView::willMount() {
+  _step = AlarmSetupStep::SETTING_HOURS;
+  Application.showStatusView();
+}
+
+void AlarmView::willUnmount() {
+  Application.hideStatusView();
+}
+
+void AlarmView::didSelect() {
+  _nextStep();
+}
+
+void AlarmView::handleScroll(ScrollEventArgs e) {
+  if (_step == AlarmSetupStep::SETTING_HOURS) {
+    if (e.delta < 0) {
+      if (_hours == 0) {
+        _hours = 23;
+      } else {
+        _hours -= 1;
+      }
+    } else {
+      if (_hours == 23) {
+        _hours = 0;
+      } else {
+        _hours += 1;
+      }
+    }
+  } else if (_step == AlarmSetupStep::SETTING_MINUTES) {
+    if (e.delta < 0) {
+      if (_minutes == 0) {
+        _minutes = 55;
+      } else {
+        _minutes -= 5;
+      }
+    } else {
+      if (_minutes == 55) {
+        _minutes = 0;
+      } else {
+        _minutes += 5;
+      }
+    }
+  } else if (_step == AlarmSetupStep::SETTING_MODE) {
+    if (e.delta < 0) {
+      if (_mode == 0) {
+        _mode = 2;
+      } else {
+        _mode -= 1;
+      }
+    } else {
+      if (_mode == 2) {
+        _mode = 0;
+      } else {
+        _mode += 1;
+      }
+    }
+  }
+  setDirty();
+}
+
+void AlarmView::render(CanvasContext *context) {
+  context->setFontSize(FontSize::H1);
+
+  context->setTextAlign(TextAlign::CENTER);
+  context->drawString(":", getClientWidth() / 2, 6);
+
+  if (_step == AlarmSetupStep::SETTING_HOURS) {
+    context->setColor(Color::WHITE);
+    context->fillRect(getClientWidth() / 2 - 36, 8, 32, 24);
+    context->setColor(Color::BLACK);
+  } else {
+    context->setColor(Color::WHITE);
+  }
+  context->setTextAlign(TextAlign::RIGHT);
+  context->drawString((_hours < 10 ? "0" : "") + String(_hours), getClientWidth() / 2 - 7, 6);
+
+  if (_step == AlarmSetupStep::SETTING_MINUTES) {
+    context->setColor(Color::WHITE);
+    context->fillRect(getClientWidth() / 2 + 4, 8, 32, 24);
+    context->setColor(Color::BLACK);
+  } else {
+    context->setColor(Color::WHITE);
+  }
+  context->setTextAlign(TextAlign::LEFT);
+  context->drawString((_minutes < 10 ? "0" : "") + String(_minutes), getClientWidth() / 2 + 7, 6);
+
+  if (_step == AlarmSetupStep::SETTING_MODE) {
+    context->setColor(Color::WHITE);
+    context->fillRect(getClientWidth() / 2 - 32, 36, 64, 16);
+    context->setColor(Color::BLACK);
+  } else {
+    context->setColor(Color::WHITE);
+  }
+  context->setTextAlign(TextAlign::CENTER);
+  context->setFontSize(FontSize::NORMAL);
+  context->drawString(ALARM_MODES[_mode], getClientWidth() / 2, 38);
+}
+
+void AlarmView::_nextStep() {
+  if (_step == AlarmSetupStep::SETTING_HOURS) {
+    _step = AlarmSetupStep::SETTING_MINUTES;
+    setDirty();
+  } else if (_step == AlarmSetupStep::SETTING_MINUTES) {
+    _step = AlarmSetupStep::SETTING_MODE;
+    setDirty();
+  } else {
+    Application.popView();
+  }
+}
---
+++ src/examples/home/views/AlarmView.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <Arduino.h>
+
+#include "../../../espix-design.h"
+
+enum class AlarmSetupStep {
+  SETTING_HOURS,
+  SETTING_MINUTES,
+  SETTING_MODE
+};
+
+class AlarmView : public View {
+public:
+  AlarmView();
+
+  static AlarmView *getInstance();
+
+  void willMount();
+  void willUnmount();
+  void handleScroll(ScrollEventArgs e);
+  void didSelect();
+
+  void render(CanvasContext *context);
+
+private:
+  const String ALARM_MODES[3] = { "EVERYDAY", "WEEKDAYS", "OFF" };
+
+  uint8_t _hours = 0;
+  uint8_t _minutes = 0;
+  uint8_t _mode = 0;
+  AlarmSetupStep _step = AlarmSetupStep::SETTING_HOURS;
+
+  void _nextStep();
+};
